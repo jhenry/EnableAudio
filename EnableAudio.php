269d593499ee10c794bf024a14c0c87e1e05e13f
CTL Web Development <ctldev@silk22.uvm.edu>
2020-09-27 11:53:40 -0400

    Initial commit

---
+++ EnableAudio.php
@@ -0,0 +1,218 @@
+<?php
+
+class EnableAudio extends PluginAbstract
+{
+	/**
+	 * @var string Name of plugin
+	 */
+	public $name = 'EnableAudio';
+
+	/**
+	 * @var string Description of plugin
+	 */
+	public $description = 'Add support for audio (i.e. mp3) files when uploading media. Based on work by Wes Wright.';
+
+	/**
+	 * @var string Name of plugin author
+	 */
+	public $author = 'Justin Henry';
+
+	/**
+	 * @var string URL to plugin's website
+	 */
+	public $url = 'https://uvm.edu/~jhenry/';
+
+	/**
+	 * @var string Current version of plugin
+	 */
+	public $version = '0.0.1';
+	/**
+	 * Performs install operations for plugin. Called when user clicks install
+	 * plugin in admin panel.
+	 *
+	 */
+	public function install()
+	{
+		$config = Registry::get('config');
+		$formats = $config->acceptedVideoFormats;
+		array_push($formats, 'mp3');
+		Registry::set('config', $formats);
+
+		Filesystem::createDir(UPLOAD_PATH . '/mp3/');
+	}
+	/**
+	 * Attaches plugin methods to hooks in code base
+	 */
+	public function load()
+	{
+		// Starting at top of upload completion controller, 
+		// b/c we still have a videoId in the session vars
+		Plugin::attachEvent('upload_complete.start', array(__CLASS__, 'processAudio'));
+	}
+
+	/**
+	 * Handle audio file uploads
+	 * 
+	 */
+	public static function processAudio()
+	{
+		EnableAudio::getVideoFromSession();
+		EnableAudio::verifyFile($video);
+		if (strtolower($video->originalExtension) == 'mp3') {
+			EnableAudio::processMp3($video);
+		}
+
+	}
+	
+	/**
+	 * Get Video object based on uploaded video in _SESSION
+	 * 
+	 */
+	private static function getVideoFromSession()
+	{
+		$videoMapper = new VideoMapper();
+
+		if (isset($_SESSION['upload']->videoId)) {
+			$video_id = $_SESSION['upload']->videoId;
+			$video = $videoMapper->getVideoById($video_id);
+			return $video;
+		}
+	}
+
+
+	/**
+	 * Handle mp3 post-upload tasks.
+	 * 
+	 */
+	public static function processMp3($video)
+	{
+		$mp3FilePath =  UPLOAD_PATH . '/mp3/' . $video->filename . '.mp3';
+		$rawVideo = UPLOAD_PATH . '/temp/' . $video->filename . '.' . $video->originalExtension;
+		EnableAudio::moveAudioFile($rawVideo, $mp3FilePath, $video);
+		EnableAudio::saveAudioInfo($rawVideo, $video);
+		EnableAudio::cleanup($rawVideo, $video);
+
+	}
+
+	/**
+	 * Get the duration for an audio file. 
+	 * 
+	 */
+	public static function getDuration($rawVideo, $video)
+	{
+		EnableAudio::debugLogMessage("\nRetrieving audio duration...");
+
+		// Retrieve duration of raw video file.
+		$ffmpegPath = Settings::get('ffmpeg');
+		$durationCommand = "$ffmpegPath -i $rawVideo 2>&1 | grep Duration:";
+		exec($durationCommand, $durationResults);
+
+		$formatResults = print_r($durationResults, TRUE);
+		EnableAudio::debugLogMessage( "Duration command results:\n" . $formatResults );
+
+		$durationResultsCleaned = preg_replace('/^\s*Duration:\s*/', '', $durationResults[0]);
+		preg_match ('/^[0-9]{2}:[0-9]{2}:[0-9]{2}/', $durationResultsCleaned, $duration);
+		$sec = Functions::durationInSeconds($duration[0]);
+
+		// Debug Log
+		EnableAudio::debugLogMessage("Duration in Seconds: $sec");
+		$formattedDuration = Functions::formatDuration($duration[0]);
+		return $formattedDuration;
+	}
+	
+	/**
+	 * Move file to final destination.
+	 * 
+	 */
+	public static function moveAudioFile($rawFilePath, $newPath, $video)
+	{
+		EnableAudio::debugLogMessage("\nCopying file...");
+		if (!copy($rawFilePath,$newPath)) {
+			throw new Exception("error copying The raw mp3 file $rawFilePath to $newPath . The id of the video is: $video->videoId $video->title");
+		}
+	
+	}
+
+	/**
+	 * Update database with info on the new upload.
+	 * 
+	 */
+	public static function saveAudioInfo($rawFilePath, $video)
+	{
+		EnableAudio::debugLogMessage("\nUpdating video information...");
+
+		// Update database with new video status information
+		$video->duration = EnableAudio::getDuration($rawFilePath, $video);
+		$videoMapper->save($video);
+
+		// Activate video
+		$videoService = new VideoService();
+		$videoService->approve($video, 'activate');
+	
+	}
+
+	/**
+	 * Clean up logs and temp/original uploads. 
+	 * 
+	 */
+	public static function cleanup($rawVideo, $video)
+	{
+		$config = Registry::get('config');
+		try {
+			// Delete original video
+			if (Settings::get('keep_original_video') != '1') {
+				EnableAudio::debugLogMessage("\nDeleting raw video...");
+				Filesystem::delete($rawVideo);
+			}
+
+			// Delete encoding log files
+			if ($config->debugConversion) {
+				EnableAudio::debugLogMessage("\nVideo ID: $video->videoId $video->title, has completed processing!\n");
+			} else {
+				Filesystem::delete($debugLog);
+			}
+		} catch (Exception $e) {
+			App::alert('Error During audio Encoding', $e->getMessage());
+			App::log(CONVERSION_LOG, $e->getMessage());
+		}
+	}
+
+
+	/**
+	 * Confirm size and existence of uploaded media.
+	 * 
+	 */
+	public static function verifyAudioFile($video)
+	{
+		$config = Registry::get('config');
+		$rawVideo = UPLOAD_PATH . '/temp/' . $video->filename . '.' . $video->originalExtension;
+		EnableAudio::debugLogMessage('Verifying raw file exists...');
+
+		if (!file_exists ($rawVideo)) {
+			throw new Exception("The raw file $rawVideo does not exist. The id of the file is: $video->videoId $video->title");
+		}
+
+		EnableAudio::debugLogMessage('Verifying raw file upload was valid size...');
+
+		// Greater than min. 5KB, anything smaller is probably corrupted
+		if (!filesize ($rawVideo) > 1024*5) {
+			throw new Exception("The raw file upload is not a valid filesize. The id of the video is: $video->videoId $video->title");
+		}
+
+	}
+
+	/**
+	 * Format and output log info for a simple string.
+	 * 
+	 * @param string $message log content
+	 * 
+	 */
+	private static function debugLogMessage($message)
+	{
+		$config = Registry::get('config');
+		$config->debugConversion ? App::log(CONVERSION_LOG, $message) : null;
+	}
+}
+
+
+?>
---
+++ README.md
@@ -0,0 +1,5 @@
+#  Enable audio file uploads
+
+Allow files such as mp3's to be uploaded and streamed.
+
+
